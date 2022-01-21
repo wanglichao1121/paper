42471f5c5fe484e30e786398832ca39fc5554f80
汪立超 <wanglichao.1121@163.com>
2022-01-21 16:02:45 +0800

    自定义title

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="sidebar">
     <div class="main-card">
-      hi
+      <MyTitle text="测试流程"/>
+      <Steps id="steps" direction="vertical">
+        <Step title="选择测试卷" description="文案"></Step>
+        <Step title="选择专长方向" description="文案"></Step>
+        <Step title="完成测试" description="文案"></Step>
+      </Steps>
     </div>
   </div>
 </template>
+<script lang="ts">
+import {Steps} from 'ant-design-vue'
+import MyTitle from '@/components/Title.vue'
+export default {
+  components: {
+    Steps,
+    Step: Steps.Step,
+    MyTitle
+  }
+}
+</script>
+<style lang="scss" scoped>
+#steps {
+  height: 300px;
+}
+</style>
+
 <style lang="scss" scoped>
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>{{text}}</p>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    props: {
+        text: String
+    }
+})
+</script>
+<style lang="scss" scoped>
+p {
+    border-left: 3px solid black;
+    align-self: flex-start;
+    font-size: 24px;
+    padding-left: 8px;
+}
+</style>
